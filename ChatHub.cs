880e3cde6d3a43314f48d805586a783d6c2fc3e4
Brennan Conroy <brecon@microsoft.com>
2019-11-18 14:33:44 -0800

    Init

---
+++ ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace WebApplication3
+{
+    public class ChatHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.Client(Context.ConnectionId).SendAsync("ReceiveMessage", "Connected");
+        }
+
+        public async Task SendMessage(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
